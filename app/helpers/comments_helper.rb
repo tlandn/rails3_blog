194af85286f53d4479c4d4a9bb4923ffc6ae1bad
Lan Tra <tra.thanhlan@gnt.com.vn>
2010-07-02 16:07:44 +0700

    Fix bugs

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def can_destroy_comment(post)
+    return current_user.id == post.user_id
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def authorname(post)
+    return "" if post.user_id.blank?
+    user = User.find(post.user_id)
+    user.login
+  end
+
+
+  def owner(post)
+    return current_user.id == post.user_id
+  end
 end
